2e80285e5d0d3af99b23e06d7807edd0838e124b
Blatacharry <fredobiriyeboah@gmail.com>
2020-01-28 13:25:50 +0000

    Btn state changes and commits added

--- src/view/App/App.js
+++ src/view/App/App.js
@@ -6,16 +6,23 @@ import Todo from '../Component/Todo/Todo';
 import UserInput from "../Component/UserInput/UserInput";
 
 class App extends Component{
+    //Get todo list from localStorage...
     setTodoList = () => {
         return localStorage.getItem('todoList') ? JSON.parse(localStorage.getItem('todoList')) : [];
     };
 
+    //Set initial state for the APP container..
     state = {
         todoList: this.setTodoList(),
         btnState: true,
         inputText: ''
     };
 
+    /*Onchange of input box...
+    * 1. Check if text is > 2
+    * 2. Activate button if true
+    * 3. Disable if false...
+    * */
     onChangeInputHandler = (event) => {
         this.setState({
             inputText: event.target.value
@@ -23,6 +30,10 @@ class App extends Component{
         return event.target.value.length > 2 ? this.setState( {btnState: false} ) : this.setState( {btnState: true} );
     };
 
+    /*
+    * Add a new list to the todos..
+    * Reset the localStorage data...
+    * */
     addTodoList = () => {
         let old_list = this.state.todoList;
 
@@ -31,17 +42,22 @@ class App extends Component{
             name: this.state.inputText,
             checked: false
         };
+
         (old_list).push(item);
         localStorage.setItem('todoList' , JSON.stringify(old_list));
 
         this.setState({
             todoList: this.setTodoList(),
-            inputText: ''
+            inputText: '',
+            btnState: true
         });
     };
 
+    /*
+    * Delete a todo item and reset the localStorage...
+    * */
     deleteTodoList = itemId => {
-        let old_list = this.state.todoList;
+        let old_list = [...this.state.todoList];
 
         const result = old_list.filter(item => item.id !== itemId);
 
@@ -53,6 +69,10 @@ class App extends Component{
         });
     };
 
+    /*
+    * Change the switch handler for a todo item...
+    * This updates the list and set value to true
+    * */
     changeSwitchHandler = itemId => {
         let old_list = this.state.todoList;
 
@@ -70,6 +90,10 @@ class App extends Component{
         });
     };
 
+    /*
+    * Edit an item...
+    * Pass item id and event
+    * */
     editItemHandler = (itemId , event) => {
         let old_list = this.state.todoList;
 
@@ -86,8 +110,6 @@ class App extends Component{
             inputText: ''
         });
     };
-
-    //changeButtonState = () => this.setState( {btnState: false} );
 
     render(){
         return (
